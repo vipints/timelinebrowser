fab522c2d8e1f4d2470b7a57e7a57cc46607f21e
Vipin <vipin@cbio.mskcc.org>
2014-11-02 19:27:33 -0500

    changed the default message to index page

--- search/views.py
+++ search/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Similarity Search index.")
-    #return render(request, 'tracks/index.html')
+    #return HttpResponse("Hello, world. You're at the Similarity Search index.")
+    return render(request, 'search/index.html')
