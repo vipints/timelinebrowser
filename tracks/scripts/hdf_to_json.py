a3db4103ce30c54345e55b68bae577aed6e99f1d
Gunnar Raetsch <raetsch@cbio.mskcc.org>
2014-11-05 12:16:25 -0500

    some more json file + the script to generate them

--- tracks/scripts/hdf_to_json.py
+++ tracks/scripts/hdf_to_json.py
@@ -20,35 +20,45 @@ def init_rec():
 
     return rec_det
 
-h5_fname = '/cbio/grlab/home/vipin/documents/BREAST_TIMELINE_SAMPLE_PD.h5'
+h5_fname = '/cbio/grlab/clinical/projects/BigData/sandbox_vipin/BREAST_TIMELINE_SAMPLE_PD.h5'
 
 records = pd.read_hdf(h5_fname, "BR_ORDER_SAMPLE")
 
-# small records 
-#patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == 1033138]
+ptids=set(records['ORD_PT_DEIDENTIFICATION_ID'])
+for i in ptids:
 
-# large records  
-#patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == 993384]
+    # small records 
+    #patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == 1033138]
 
-patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == 1540879]
+    # large records  
+    #patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == 993384]
 
-data = [] 
-counter = 1 
-for count, row in patient_1_rec.iterrows():
+    patient_1_rec = records[records['ORD_PT_DEIDENTIFICATION_ID'] == i]
 
-    rec = init_rec()
+    data = [] 
+    counter = 1 
+    for count, row in patient_1_rec.iterrows():
 
-    rec['eventId'] = counter 
-    counter += 1 
+        rec = init_rec()
 
-    rec['patientId'] = row['ORD_PT_DEIDENTIFICATION_ID']
-    rec['eventType'] = row['ORD_TYPE_CD'].strip( ' ' )
-    rec['startDate'] = row['ORD_DAYS_SINCE_MRN_CREATE_DTE']
-    rec['eventData'] = row['ORD_NAME'].strip( ' ' )
-    rec['eventMon'] = row['ORD_MONTH']
-    rec['eventYear'] = row['ORD_YEAR']
+        rec['eventId'] = counter 
+        counter += 1 
 
-    data.append(rec) 
+        rec['patientId'] = row['ORD_PT_DEIDENTIFICATION_ID']
+        rec['eventType'] = row['ORD_TYPE_CD'].strip( ' ' )
+        rec['startDate'] = row['ORD_DAYS_SINCE_MRN_CREATE_DTE']
+        rec['eventData'] = row['ORD_NAME'].strip( ' ' )
+        rec['eventMon'] = row['ORD_MONTH']
+        rec['eventYear'] = row['ORD_YEAR']
 
-json_data = json.dumps(data)
-print json_data 
+        data.append(rec) 
+
+    json_data = json.dumps(data)
+
+    #print json_data 
+
+    json_fn='../static/tracks/tmp/dataset_%i.json' % rec['patientId']
+    print json_fn
+    fd=open(json_fn, 'w+') ;
+    fd.write(json_data)
+    fd.close()
